1be1d6a5f9db75a6c30513d719d8067158ebe56b
Nikolas Figueroa <nikolasfigueroa10@gmail.com>
2018-04-27 12:56:29 +0000

    AJAX

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 $(document).ready(function(){
-  
-  
-  
+   $.ajax({
+  url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+  method: "GET",
+  success: function(response) {
+    //Below log the response object to the console
+    console.log(response);
+  }
+  });
   
 });
